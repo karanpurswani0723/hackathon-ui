d106b17da3773a5f25d7cbdfd4ae0dfc55d3ef46
hardik2598 <hardikbhatt2598@gmail.com>
2019-10-10 03:16:14 +0530

    Updated Registration Form

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -11,7 +11,7 @@ class HomePage extends React.Component {
       firstName: "",
       lastName: "",
       email: "",
-      mobileNumber: ""
+      mobileNumber: "",
     },
     loginStatus: true
   };
